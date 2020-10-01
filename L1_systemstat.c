f3622812a10000044cbb3602b004e21bd31b8945
genericbrian <51295695+genericbrian@users.noreply.github.com>
2020-10-01 20:50:48 +0530

    Update L1_systemstat.c

--- L1_systemstat.c
+++ L1_systemstat.c
@@ -1,7 +1,8 @@
 // Lab 1 - Print System Statistics 
 
 #include <stdio.h>
-#include <sys/sysinfo.h>
+#include <sys/sysinfo.h> // Sys Stats
+#include<sys/utsname.h>  //Sys Info
 
 int main ()
 {
@@ -10,6 +11,7 @@ int main ()
  const long day = hour * 24;
  const double megabyte = 1024 * 1024;
  
+ // System Stats 
  struct sysinfo si;
  sysinfo (&si);
  
@@ -17,5 +19,17 @@ int main ()
  printf ("total RAM   : %5.1f MB\n", si.totalram / megabyte);
  printf ("free RAM   : %5.1f MB\n", si.freeram / megabyte);
  printf ("process count : %d\n", si.procs);
+ 
+ // System info
+ struct utsname uname_pointer;
+
+ uname(&uname_pointer);
+
+ printf("System name - %s \n", uname_pointer.sysname);
+ printf("Nodename    - %s \n", uname_pointer.nodename);
+ printf("Release     - %s \n", uname_pointer.release);
+ printf("Version     - %s \n", uname_pointer.version);
+ printf("Machine     - %s \n", uname_pointer.machine);
+ printf("Domain name - %s \n", uname_pointer.__domainname);
  return 0;
 }
